be72c75f1173473d0eda04fbcccdcf0f85648c32
pabgarcialopez <pabgarcialopez@gmail.com>
2025-03-27 17:27:53 +0100

    Rename plot_spectogram to plot_mel_spectogram for clarity and update comments

--- src/utils/audio_utils.py
+++ src/utils/audio_utils.py
@@ -10,11 +10,9 @@ def plot_waveform(waveform):
     plt.ylabel("Amplitude")
     plt.show()
 
-def plot_spectogram(mel_spec_db):
-    # 6) Plot del espectrograma
+def plot_mel_spectogram(mel_spec_db):
     plt.figure(figsize=(10, 4))
     plt.title("Mel Spectrogram")
-    # mel_spec_db.shape = [1, 64, time_frames], erase channel dimension with .squeeze(0)
     plt.imshow(mel_spec_db.squeeze(0).numpy(), origin="lower", aspect="auto")
     plt.colorbar(label="dB")
     plt.xlabel("Time frames")
